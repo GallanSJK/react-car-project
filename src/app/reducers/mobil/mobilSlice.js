cb512cfa4e1b825676b9c7fc4d63429a3e196968
GallanSJK <gallansteffan99@gmail.com>
2023-05-06 18:43:38 +0700

    Adding Filter Date, Type Driver and Capacity

--- src/app/reducers/mobil/mobilSlice.js
+++ src/app/reducers/mobil/mobilSlice.js
@@ -1,6 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { fetchMobilApi } from './mobilApi';
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 export const fetchMobil = createAsyncThunk(
     'mobil/fetchMobil',
     async() => {
@@ -9,36 +16,47 @@ export const fetchMobil = createAsyncThunk(
     }
 )
 
-// export const filteMobil = createAsyncThunk(
-//   'mobil/filterMobil',
-//   async(payload) => {
-//       const res = fetchMobilApi(payload);
-//       return res
-//   }
-// )
+export const filteMobil = createAsyncThunk(
+  'mobil/filterMobil',
+  async(payload) => {
+      const res = fetchMobilApi(payload);
+      return res
+  }
+)
 
 export const mobilSlice = createSlice({
     name: 'mobil',
     initialState: {
         data: [],
+        initData: [],
         loading: 'idle',
         message: ''
     },
     reducers: {
-      // filterMobil: (state, action) => {
-      //   const filters = action.payload
-      //   state.data = state.initData.filter(el => {
-      //       return Object.keys(filters).every(filter => {
-      //           if(filter === 'availableAt') {
-      //               const d1 = new Date(filters[filter]);
-      //               const d2 = new Date(el[filter]);
-      //               console.log(d1, d2)
-      //               return d1.getTime() >= d2.getTime() 
-      //           }
-      //           return filters[filter] === el[filter]
-      //       });
-      //   })
-      // },
+      filterMobil: (state, action) => {
+        const filters = action.payload
+        state.data = state.initData.filter(el => {
+            return Object.keys(filters).every(filter => {
+              let availableAt = true,
+              capacity = true,
+              typeDriver = true;
+                if(filter === 'availableAt') {
+                    const d1 = new Date(filters[filter]);
+                    const d2 = new Date(el[filter]);
+                    console.log(d1, d2)
+                    // return d1.getTime() >= d2.getTime() 
+                    availableAt = d1.getTime() >= d2.getTime() 
+                }
+                if(filter === 'capacity') {
+                  capacity = el[filter] >= filters[filter]
+                }
+                if(filter === 'typeDriver') {
+                  typeDriver = el[filter] >= filters[filter]
+                }
+                return capacity && availableAt && typeDriver
+            });
+        })
+      },
     },
     extraReducers: (builder) => {
         builder
@@ -47,8 +65,22 @@ export const mobilSlice = createSlice({
             })
             .addCase(fetchMobil.fulfilled, (state, action) => {
                 state.loading = 'idle';
-                state.data = action.payload
-                // state.initData = action.payload
+                const cars = action.payload.map((car) => {
+                  const isPositive = getRandomInt(0, 1) === 1;
+                  const timeAt = new Date();
+                  const mutator = getRandomInt(1000000, 100000000);
+                  const availableAt = String(new Date(timeAt.getTime() + (isPositive ? mutator : -1 * mutator)))
+                  const typeDriver = isPositive ? 'Lepas Kunci' : 'Dengan Supir'
+                  const capacity = getRandomInt(2, 8)
+                  return {
+                    ...car,
+                    capacity,
+                    typeDriver,
+                    availableAt,
+                  };
+                })
+                state.data = cars
+                state.initData = cars
             })
             .addCase(fetchMobil.rejected, (state, action) => {
                 state.loading = 'idle';
@@ -59,5 +91,5 @@ export const mobilSlice = createSlice({
 
 export const selectMobil = (state) => state.mobil.data
 export const selectMobilLoading = (state) => state.mobil.loading
-// export const { filterMobil } = mobilSlice.actions
+export const { filterMobil } = mobilSlice.actions
 export default mobilSlice.reducer
--- src/pages/Carimobil/index.jsx
+++ src/pages/Carimobil/index.jsx
@@ -13,6 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {
   fetchMobil,
   selectMobil,
+  filterMobil,
   selectMobilLoading
 } from '../../app/reducers/mobil/mobilSlice'
 
@@ -21,9 +22,11 @@ export default function Carimobil() {
   const firstLoad = useRef(true)
   const mobil = useSelector(selectMobil)
   const mobilLoading = useSelector(selectMobilLoading)
-  // const [filter, setFilter] = useState({
-  //   availableAt: ''
-  // })
+  const [filter, setFilter] = useState({
+    availableAt: '',
+    capacity: 0,
+    typeDriver: '',
+  })
 
   const dispatch = useDispatch();
 
@@ -34,18 +37,18 @@ export default function Carimobil() {
     }
   }, [])
 
-  // const handleChange = (e) => {
-  //   setFilter({
-  //       ...filter,
-  //       [e.target.name]: e.target.value 
-  //   })
-  // }
+  const handleChange = (e) => {
+    setFilter({
+        ...filter,
+        [e.target.name]: e.target.value 
+    })
+  }
 
-  // const handleFilter = (e) => {
-  //   e.preventDefault()
+  const handleFilter = (e) => {
+    e.preventDefault()
 
-  //   dispatch(filterMobil(filter))
-  // }
+    dispatch(filterMobil(filter))
+  }
 
   // useEffect(() => {
   //   console.log(data)
@@ -55,20 +58,16 @@ export default function Carimobil() {
         <>
             <Header />
             <Cover />
-            {/* <form onSubmit={(e) => handleFilter(e)}>
-                <input type="date" name="availableAt" onChange={(e) => handleChange(e)}/>
-                <button type='submit'>Search</button>
-            </form> */}
             <section className="Carimobil">
                 <Container>
                     <Row className="justify-content-center">
                         <Col lg={12}>
-                        {/* <Form onSubmit={(e) => handleFilter(e)}></Form> */}
+                        <Form onSubmit={(e) => handleFilter(e)}>
                         <div className="shadow p-3 rounded">
                           <Row className="row-cols-lg-auto g-0 justify-content-around">
                             <Col xs={12} lg={2}>
                               <label id="tipedriver" htmlFor="exampleDataList" className="form-label">Tipe Driver</label>
-                              <Form.Select  aria-label="Default select example">
+                              <Form.Select name="typeDriver" aria-label="Default select example" onChange={(e) => handleChange(e)}>
                                 <option value="0">Pilih Tipe Driver</option>
                                 <option value="Dengan Supir">Dengan Sopir</option>
                                 <option value="Lepas Kunci">Tanpa Sopir (Lepas Kunci)</option>
@@ -77,8 +76,7 @@ export default function Carimobil() {
 
                             <Col xs={12} lg={2}>
                               <label htmlFor="exampleDataList" className="form-label">Tanggal</label>
-                              <Form.Control type="date" id="tersedia" name="availableAt" placeholder="Select Date" />
-                              {/* <Form.Control type="date" id="tersedia" name="availableAt" onChange={(e) => handleChange(e)} /> */}
+                              <Form.Control type="date" id="tersedia" name="availableAt" onChange={(e) => handleChange(e)} />
                             </Col>
 
                             <Col xs={12} lg={2}>
@@ -88,7 +86,7 @@ export default function Carimobil() {
 
                             <Col xs={12} lg={3}>
                               <label htmlFor="exampleDataList" className="form-label">Jumlah Penumpang (Optional)</label>
-                              <Form.Control type="text" id="penumpang" placeholder="Jumlah Penumpang" />
+                              <Form.Control type="text" id="penumpang" name="capacity" onChange={(e) => handleChange(e)} />
                             </Col>        
                                                 
                             <Button id="filter" type="submit" className="btn-filter">
@@ -96,7 +94,8 @@ export default function Carimobil() {
                             </Button>
                             
                             </Row>
-                        </div>           
+                        </div>
+                        </Form>           
                         </Col>
                     </Row>
                 </Container>
@@ -104,7 +103,7 @@ export default function Carimobil() {
             <div>
                 { mobilLoading === 'loading' && <div>Loading...</div>}
                 <Container>
-                  <Row className='mt-4' gap={4}>          
+                  <Row className='mt-4 g-4'>          
                 { mobil ? mobil.map((e,i) => (
                   
                   <Col md={6} lg={4} key={i}>
@@ -115,11 +114,13 @@ export default function Carimobil() {
                         <h4><b>Rp. {e.rentPerDay} / Hari</b></h4>
 
                         <p>Plat Nomer: <b>{e.plate}</b></p>
-                        {/* <p>Tipe Driver: <b>{e.typeDriver}</b></p> */}
+                        <p>Tipe Driver: <b>{e.typeDriver}</b></p>
                         <p><b>{e.description}</b></p>
                         <p>Kapasitas : <b>{e.capacity}</b></p>
                         <p>Transmisi : <b>{e.transmission}</b></p>
                         <p>Tahun : <b>{e.year}</b></p>
+                        <p>Tersedia : <b>{e.availableAt}</b></p>
+
 
                         <Button className='btn-pilih-mobil'>Pilih Mobil</Button>
                       </Card.Body>
